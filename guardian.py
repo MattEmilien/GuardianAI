95f415e3d72785300035bd2f5719f3825491e323
Matthew Emilien <105692955+MattEmilien@users.noreply.github.com>
2023-09-05 13:02:48 -0500

    [+]  Updated GUI layout
    [-] Tkinter
    [+] CustomTkinter
    [+] Pywin32

--- guardian.py
+++ guardian.py
@@ -1,23 +1,20 @@
 import sys
 import os
 from pyuac import main_requires_admin
-from gui.main_window import MainWindow
+from gui.main_window import AntivirusApp
+
 
 @main_requires_admin
 def main():
-    # Check if the application is running with administrative privileges (Windows)
-    # if os.name == 'nt' and not os.environ.get('APP_ADMIN'):
-    #     # Relaunch the application with administrative privileges
-    #     run_as_admin()
-    #     sys.exit(0)
+    app = AntivirusApp()
+    app.mainloop()
 
-    # Initialize and start the GUI
-    app = MainWindow()
-    app.run()
 
 def run_as_admin():
     import ctypes
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+    ctypes.windll.shell32.ShellExecuteW(
+        None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+
 
 if __name__ == "__main__":
     main()
--- gui/main_window.py
+++ gui/main_window.py
@@ -1,62 +1,112 @@
 import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
+import customtkinter as ctk
 
-class MainWindow:
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue")
+
+
+class AntivirusApp(ctk.CTk):
     def __init__(self):
-        # Initialize the main window
-        self.root = tk.Tk()
-        self.root.title("Guardian AI")
-        self.root.geometry("800x600")
+        super().__init__()
+        self.title("Guardian AI - Antivirus")
+        self.geometry("1100x580")
 
-        # Create a menu bar
-        self.create_menu_bar()
+        self.create_ui()
 
-        # Create and configure main content frame
-        self.main_frame = ttk.Frame(self.root, padding=(10, 10))
-        self.main_frame.grid(column=0, row=0, sticky=(tk.W, tk.N, tk.E, tk.S))
-        self.main_frame.columnconfigure(0, weight=1)
-        self.main_frame.rowconfigure(0, weight=1)
+    def create_ui(self):
+        self.configure_grid()
+        self.create_main_frame()
+        self.create_sidebar()
+        self.create_quarantined_items()
+        self.center_main_frame()
 
-        # Add widgets and components to the main frame
-        self.create_widgets()
+    def configure_grid(self):
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
-    def create_menu_bar(self):
-        # Create a menu bar
-        self.menu_bar = tk.Menu(self.root)
-        self.root.config(menu=self.menu_bar)
+    def create_main_frame(self):
+        self.main_frame = ctk.CTkFrame(self)
+        self.main_frame.grid(row=0, column=0, sticky="nsew")
 
-        # Create a "File" menu
-        file_menu = tk.Menu(self.menu_bar, tearoff=0)
-        self.menu_bar.add_cascade(label="File", menu=file_menu)
-        file_menu.add_command(label="Open File", command=self.open_file)
-        file_menu.add_separator()
-        file_menu.add_command(label="Exit", command=self.root.quit)
+    def create_sidebar(self):
+        sidebar_frame = ctk.CTkFrame(self.main_frame, width=140)
+        sidebar_frame.grid(row=0, column=0, rowspan=2, sticky="nsew")
+        sidebar_frame.grid_rowconfigure(2, weight=1)
 
-    def create_widgets(self):
-        # Add your GUI components and widgets here
-        # Example: Create labels, buttons, frames, etc.
+        ctk.CTkLabel(sidebar_frame, text="Guardian AI - Antivirus",
+                     font=ctk.CTkFont(size=20, weight="bold")).grid(row=0, column=0, padx=20, pady=(20, 10))
 
-        # Example label
-        label = ttk.Label(self.main_frame, text="Welcome to Guardian AI!")
-        label.grid(column=0, row=0)
+        scan_button = ctk.CTkButton(
+            sidebar_frame, text="Scan", command=self.scan)
+        scan_button.grid(row=1, column=0, padx=20, pady=10)
+        update_button = ctk.CTkButton(
+            sidebar_frame, text="Update Definitions", command=self.update_definitions)
+        update_button.grid(row=2, column=0, padx=20, pady=10)
 
-        # Example button
-        button = ttk.Button(self.main_frame, text="Scan", command=self.scan)
-        button.grid(column=0, row=1)
+        appearance_label = ctk.CTkLabel(
+            sidebar_frame, text="Appearance Mode:", anchor="w")
+        appearance_label.grid(row=5, column=0, padx=20, pady=(10, 0))
 
-    def run(self):
-        # Start the main GUI loop
-        self.root.mainloop()
+        appearance_optionmenu = ctk.CTkOptionMenu(sidebar_frame, values=["Light", "Dark", "System"],
+                                                  command=self.change_appearance_mode_event)
+        appearance_optionmenu.grid(row=6, column=0, padx=20, pady=(10, 10))
 
-    def open_file(self):
-        # Implement file opening functionality here
-        pass
+        scaling_label = ctk.CTkLabel(
+            sidebar_frame, text="UI Scaling:", anchor="w")
+        scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
+
+        scaling_optionmenu = ctk.CTkOptionMenu(sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
+                                               command=self.change_scaling_event)
+        scaling_optionmenu.grid(row=8, column=0, padx=20, pady=(10, 20))
+
+        self.scan_button = scan_button
+        self.scan_progress = None
+
+    def create_quarantined_items(self):
+        items_frame = ctk.CTkScrollableFrame(
+            self.main_frame, label_text="Quarantined Items")
+        items_frame.grid(row=0, column=1, padx=(
+            20, 0), pady=(20, 0), sticky="nsew")
+
+        items_frame.grid_rowconfigure(0, weight=1)
+        items_frame.grid_columnconfigure(0, weight=1)
+
+        # Simulate adding quarantined items (Replacing after project structure changes)
+        for i in range(10):
+            ctk.CTkSwitch(master=items_frame, text=f"Item {i}").grid(
+                row=i, column=0, padx=10, pady=(0, 20))
+
+    def center_main_frame(self):
+        self.main_frame.grid_columnconfigure(0, weight=1)
+        self.main_frame.grid_rowconfigure(0, weight=1)
 
     def scan(self):
-        # Implement the scan functionality here
-        messagebox.showinfo("Scan", "Scanning for threats...")
+        self.scan_button.configure(state="disabled", text="Scanning...")
+        self.scan_progress = ctk.CTkProgressBar(self.main_frame)
+        self.scan_progress.grid(row=1, column=0, padx=20,
+                                pady=(20, 0), sticky="nsew")
+        self.scan_progress.start()
+        self.after(3000, self.stop_scan)  # Add Scanning callback func here!
+
+    def stop_scan(self):
+        if self.scan_progress:
+            self.scan_progress.stop()
+            self.scan_progress.destroy()
+            self.scan_progress = None
+
+        self.scan_button.configure(state="normal", text="Scan")
+
+    def update_definitions(self):
+        pass  # Updating this later :)
+
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        ctk.set_appearance_mode(new_appearance_mode)
+
+    def change_scaling_event(self, new_scaling: str):
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        ctk.set_widget_scaling(new_scaling_float)
+
 
 if __name__ == "__main__":
-    app = MainWindow()
-    app.run()
+    app = AntivirusApp()
+    app.mainloop()
