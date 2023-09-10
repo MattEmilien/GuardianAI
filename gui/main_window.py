3e00bc03a63fd6d254ec3c4013012c3bbdbdace8
Matthew Emilien <105692955+MattEmilien@users.noreply.github.com>
2023-09-10 18:19:56 -0500

    [+] Updated main_window.py

--- gui/main_window.py
+++ gui/main_window.py
@@ -11,7 +11,7 @@ class Main_window(customtkinter.CTk):
         self.title("Guardian AI - Antivirus")
         self.geometry("1100x580")
 
-        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)g
         self.grid_columnconfigure(1, weight=1)
 
         self.logo_image = customtkinter.CTkImage(
